85233f4a9d986067a79f95f99cdd26f0d2bd1900
CodeDragone03 <jay923060@gmail.com>
2024-12-15 00:11:20 +0530

    Replace paragraph with heading for service page and adjust font size for better visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <p
-          className="text-6xl max-w-60 font-bold italic absolute top-40 left-20 m-4 lg:top-20 lg:left-40 md:top-40 md:left-40"
+        <h1
+          className="text-6xl max-w-60 font-bold italic text-center"
           style={{
             fontFamily: "Twinkle_Star",
-            fontSize: "4rem",
+            fontSize: "6rem",
           }}
         >
           Service Page
-        </p>
+        </h1>
         <Image
           src="/Service.png"
           alt="Service Image"
