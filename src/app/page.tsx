96d2feb463ef965767ff9b34d45eaa6c983bc509
CodeDragone03 <jay923060@gmail.com>
2024-12-15 00:22:34 +0530

    Refactor home page layout by removing unused Image component and applying background image style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div
+        className="flex flex-col items-center justify-center min-h-screen py-2 "
+        style={{ backgroundImage: "./public/Service.png" }}
+      >
         <h1
           className="font-bold italic text-center"
           style={{
             fontFamily: "Twinkle_Star",
             fontSize: "6rem",
+            marginBottom: "2rem",
           }}
         >
           Down Time !
         </h1>
-        <Image
-          src="/Service.png"
-          alt="Service Image"
-          style={{
-            maxWidth: "50%",
-            maxHeight: "40%",
-            position: "relative",
-            marginBottom: "2rem",
-          }}
-        ></Image>
       </div>
     </>
   );
