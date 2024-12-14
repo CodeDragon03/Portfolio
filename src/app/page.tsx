8d9ad45c73869e7d8d35ccc6f080e95f6afe2d34
CodeDragone03 <jay923060@gmail.com>
2024-12-15 00:09:54 +0530

    Refactor layout by removing absolute positioning from text element and adjusting image style for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 relative">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <p
           className="text-6xl max-w-60 font-bold italic absolute top-40 left-20 m-4 lg:top-20 lg:left-40 md:top-40 md:left-40"
           style={{
@@ -16,7 +16,7 @@ export default function Home() {
         <Image
           src="/Service.png"
           alt="Service Image"
-          style={{ maxWidth: "40%", maxHeight: "40%" }}
+          style={{ maxWidth: "40%", maxHeight: "40%", position: "relative" }}
         ></Image>
       </div>
     </>
