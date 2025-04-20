2537d95f844b1afff1be3f6f5c8df9951c77f0b2
Jay Yadav (CodeDragon) <jay923060@gmail.com>
2025-04-20 22:13:09 +0530

    Update

--- client/src/components/other/GolbalNavigationBar.jsx
+++ client/src/components/other/GolbalNavigationBar.jsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import BrandLogo from '../ui/BrandLogo';
-import NavbarItem from '../ui/NavbarItem';
 
 const GlobalNavigationBar = () => {
     return <motion.nav className="navbar-global"></motion.nav>;
