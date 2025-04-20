c0143cc1ba3e5b01609f18f51d82c35718bbae92
Jay Yadav (CodeDragon) <jay923060@gmail.com>
2025-04-20 22:01:53 +0530

    Update

--- client/src/components/other/GolbalNavigationBar.jsx
+++ client/src/components/other/GolbalNavigationBar.jsx
@@ -1,23 +1,9 @@
 import { motion } from 'framer-motion';
-
 import BrandLogo from '../ui/BrandLogo';
 import NavbarItem from '../ui/NavbarItem';
 
 const GlobalNavigationBar = () => {
-    return (
-        <motion.nav className="navbar-global">
-            <motion.div className="navbar-logo">
-                <BrandLogo />
-            </motion.div>
-            <motion.div className="navbar-navigation">
-                <ul className="navbar-navigation-list">
-                    <NavbarItem to="/" text="Harbour" />
-                    <NavbarItem to="/case-study" text="Case Study" />
-                    <NavbarItem to="/research" text="Research" />
-                </ul>
-            </motion.div>
-        </motion.nav>
-    );
+    return <motion.nav className="navbar-global"></motion.nav>;
 };
 
 export default GlobalNavigationBar;
